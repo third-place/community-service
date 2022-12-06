29ea5ed5e9317440df6384297cfbc4015a93ec8b
Dan Munro <dan@danmunro.com>
2022-12-05 18:00:52 -0800

    uncomment

--- internal/kafka/connection.go
+++ internal/kafka/connection.go
@@ -8,10 +8,10 @@ import (
 func GetReader() (*kafka.Consumer, error) {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
-		//"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
-		//"sasl.mechanisms":   os.Getenv("KAFKA_SASL_MECHANISM"),
-		//"sasl.username":     os.Getenv("KAFKA_SASL_USERNAME"),
-		//"sasl.password":     os.Getenv("KAFKA_SASL_PASSWORD"),
+		"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
+		"sasl.mechanisms":   os.Getenv("KAFKA_SASL_MECHANISM"),
+		"sasl.username":     os.Getenv("KAFKA_SASL_USERNAME"),
+		"sasl.password":     os.Getenv("KAFKA_SASL_PASSWORD"),
 		"group.id":          "community-service",
 		"auto.offset.reset": "smallest",
 	})

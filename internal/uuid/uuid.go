67bd62bc5ed13dc830cbe2b2b7b2e4630ecb6248
Dan Munro <dan@danmunro.com>
2022-12-24 20:58:28 -0800

    dead code

--- internal/uuid/uuid.go
+++ internal/uuid/uuid.go
@@ -9,10 +9,6 @@ func GetUuidFromPathSecondPosition(path string) uuid.UUID {
 	return getUuidFromPathIndex(path, 2)
 }
 
-func GetUuidFromPathThirdPosition(path string) uuid.UUID {
-	return getUuidFromPathIndex(path, 3)
-}
-
 func getUuidFromPathIndex(path string, index int) uuid.UUID {
 	parts := strings.Split(path, "/")
 	return uuid.MustParse(parts[index])

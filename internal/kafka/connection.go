1c9800a1ffc57b7f46f0ffd0b4083aa489daf955
Dan Munro <dan@danmunro.com>
2024-02-11 09:25:51 -0600

    fix kafka config/connection

--- internal/kafka/connection.go
+++ internal/kafka/connection.go
@@ -2,19 +2,13 @@ package kafka
 
 import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"os"
 )
 
 func GetReader() (*kafka.Consumer, error) {
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
-		"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
-		"sasl.mechanisms":   os.Getenv("KAFKA_SASL_MECHANISM"),
-		"sasl.username":     os.Getenv("KAFKA_SASL_USERNAME"),
-		"sasl.password":     os.Getenv("KAFKA_SASL_PASSWORD"),
-		"group.id":          "community-service",
-		"auto.offset.reset": "smallest",
-	})
+	cfg := createConnectionConfig()
+	_ = cfg.SetKey("group.id", "community-service")
+	_ = cfg.SetKey("auto.offset.reset", "earliest")
+	c, err := kafka.NewConsumer(cfg)
 	if err != nil {
 		return nil, err
 	}

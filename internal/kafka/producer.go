ba3e2a20748d0de53fbbb3ad48fe0e7490f1f9d3
Dan Munro <dan@danmunro.com>
2024-02-20 19:42:24 -0800

    createProducer refactor

--- internal/kafka/producer.go
+++ internal/kafka/producer.go
@@ -1,28 +1,15 @@
 package kafka
 
 import (
-	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"log"
-	"os"
 )
 
 type Producer interface {
 	Produce(msg *kafka.Message, deliveryChan chan kafka.Event) error
 }
 
-func CreateProducer() Producer {
-	producer, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
-		"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
-		"sasl.mechanisms":   os.Getenv("KAFKA_SASL_MECHANISM"),
-		"sasl.username":     os.Getenv("KAFKA_SASL_USERNAME"),
-		"sasl.password":     os.Getenv("KAFKA_SASL_PASSWORD"),
-	})
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Failed to create producer: %s", err))
-	}
-	return producer
+func CreateProducer() (Producer, error) {
+	return kafka.NewProducer(createConnectionConfig())
 }
 
 func CreateMessage(data []byte, topic string) *kafka.Message {

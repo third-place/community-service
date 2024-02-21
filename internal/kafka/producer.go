8b3e3cb94c35f4350179617154d50f3ef46cdf08
Dan Munro <dan@danmunro.com>
2024-02-20 19:46:22 -0800

    fatal on error

--- internal/kafka/producer.go
+++ internal/kafka/producer.go
@@ -2,14 +2,19 @@ package kafka
 
 import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
 )
 
 type Producer interface {
 	Produce(msg *kafka.Message, deliveryChan chan kafka.Event) error
 }
 
-func CreateProducer() (Producer, error) {
-	return kafka.NewProducer(createConnectionConfig())
+func CreateProducer() Producer {
+	producer, err := kafka.NewProducer(createConnectionConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return producer
 }
 
 func CreateMessage(data []byte, topic string) *kafka.Message {
